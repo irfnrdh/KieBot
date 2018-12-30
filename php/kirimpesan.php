3f9aec96fe58fce28d38f2529e83896c2da80bd6
irfnrdh <kang.fand@gmail.com>
2018-12-30 18:38:30 +0700

    Update kirimpesan.php

--- php/kirimpesan.php
+++ php/kirimpesan.php
@@ -15,3 +15,16 @@ curl_close($ch);
 
 var_dump($api);
 
+//kalau pakai fungsi!
+function sendMessage($chatid, $pesant, $token) {
+    $url = "https://api.telegram.org/" . $secret_token . "/sendMessage?parse_mode=markdown&chat_id=" . $telegram_id;
+    $url = $url . "&text=" . urlencode($message_text);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
